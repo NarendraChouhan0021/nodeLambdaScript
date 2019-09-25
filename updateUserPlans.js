862a6e14f0208dd304f7bf5f37b442098b380f87
Narendra <narendrachouhan0021@gmail.com>
2019-09-25 21:15:28 +0530

    update  api done

--- updateUserPlans.js
+++ updateUserPlans.js
@@ -4,39 +4,25 @@ const docClient = new AWS.DynamoDB.DocumentClient();
 const updateUserPlans = async (event, context, callback) => {
 
     const { email, planId, dates } = JSON.parse(event.body);
+    let datesObjKey = Object.keys(dates)[0]
 
     try {
-        let plansRecord = await docClient.get({
+        let updatePlans = {
             TableName: 'dev-Plans',
-            IndexName: 'emailIndex',
-            Key: { email, planId }
-        }).promise();
+            Key: { email, planId },
+            UpdateExpression: "SET #dates.#objKey = :locVal",
+            ExpressionAttributeNames: {
+                '#dates': 'dates',
+                '#objKey': datesObjKey,
+            },
+            ExpressionAttributeValues: {
+                ':locVal': dates[datesObjKey]
+            },
+            ReturnValues: "UPDATED_NEW"
+        };
+        await docClient.update(updatePlans).promise();
 
-        plansRecord = plansRecord.Item
-
-       if (plansRecord.dates.hasOwnProperty(dates)) {
-            var insssss = { "reading": ["212121256789"], "complete": true }
-
-            let updatePlans = {
-                TableName: 'dev-Plans',
-                Key: { email, planId },
-                UpdateExpression: "SET #dates.#objKey = :locVal",
-                ExpressionAttributeNames: {
-                    '#dates': 'dates',
-                    '#objKey': dates,
-                },
-                ExpressionAttributeValues: {
-                    ':locVal': insssss
-                },
-                ReturnValues: "UPDATED_NEW"
-            };
-
-            await docClient.update(updatePlans).promise();
-
-            callback(null, { statusCode: 200, body: JSON.stringify("Updated Successfully") })
-        } else {
-            callback(null, { statusCode: 500, body: JSON.stringify(`couldn't update the plan dates.`) });
-        }
+        callback(null, { statusCode: 200, body: JSON.stringify("Updated Successfully") })
     } catch (err) {
         callback(null, { statusCode: 500, body: JSON.stringify(`${err} Couldn't insert data.`) });
     }
